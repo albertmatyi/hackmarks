e3ec55501ae2a00e19b0d8b64d3ad59939350f0b
albertmatyi <albertmatyi@gmail.com>
2015-09-26 18:16:19 +0200

    add user info to dashboard

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,7 +1,18 @@
 'use strict';
 
 Router.route('/', {
-   name: 'home',
+    name: 'home',
     template: 'home',
-
+    waitOn: function () {
+        return [Meteor.subscribe('teams'), Meteor.subscribe('users')];
+    },
+    data: function () {
+        var user = Meteor.user();
+        if (user) {
+            return {
+                user: user,
+                team: App.teams.collection.findOne(user.profile.teamId)
+            }
+        }
+    }
 });
