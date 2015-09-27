2cd50cb6e751f2b40ed273a3ed14941f7eee393a
albertmatyi <albertmatyi@gmail.com>
2015-09-27 04:03:42 +0200

    style team view

--- common/teams.js
+++ common/teams.js
@@ -1,14 +1,15 @@
 'use strict';
+var DEFAULT_SUMMARY = '\nOur awesome project\n========================\n\nI could write pages about it but will instead just summarize it in this short and frank sentence. How cool is that?\n\nPrerequisites\n-------------\n\n    1. Install `node`\n1. Install `grunt`\n`$> sudo npm install -g grunt-cli`\n\n1. Clone the project by\n`git clone http://from.github.lol/`\n\n    Usage\n---------\n\n    Do some stuff\n\n1. Checkout project\n1. `$> cd PROJECT_DIR`\n1. `$> npm install`\n1. `$> grunt` will launch a rocket\n\n\n## Bam it\'s on Baby! We RoCI{\n';
 
 App.component('teams').expose({
     collection: new Meteor.Collection('teams')
 });
 
-
 App.teams.collection.allow({
     insert: function (userId, doc) {
         doc.ownerId = userId;
         doc.memberIds = [userId];
+        doc.summary = DEFAULT_SUMMARY;
         var idx = App.teams.collection.find().count();
         doc.image = '/images/mood/' + (idx % 30) + '.jpg'
         var existingTeam = App.teams.collection.findOne({name: doc.name});
