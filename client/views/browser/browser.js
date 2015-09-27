e221d96b697111dda32e0a20f9e0e26b359b14ca
albertmatyi <albertmatyi@gmail.com>
2015-09-27 10:38:25 +0200

    add search functionality

--- client/views/browser/browser.js
+++ client/views/browser/browser.js
@@ -2,8 +2,19 @@
 
 Router.route('browser', {
     data: function () {
+        var selector = {};
+        var st = Session.get('browser.searchTerm');
+        if (st) {
+            selector = {
+                $or: [{
+                    name: new RegExp(st)
+                }, {
+                    tags: new RegExp(st)
+                }]
+            };
+        }
         return {
-            teams: App.teams.collection.find({}, {sort: {funds: -1}})
+            teams: App.teams.collection.find(selector, {sort: {funds: -1}})
         };
     }
 });
@@ -11,5 +22,11 @@ Router.route('browser', {
 Template.browser.events({
     'click .team': function (e) {
         Router.go('team', this);
+    },
+    'keyup .searchbar .form-control': function (e) {
+        var searchTerm = $(e.currentTarget).val();
+        searchTerm = searchTerm.trim();
+        Session.set('browser.searchTerm', searchTerm);
     }
 });
+
